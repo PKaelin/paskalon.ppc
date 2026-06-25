cb8e56c6fbd6707297733893cd48c8392b25a4e6
PKaelin <42666478+PKaelin@users.noreply.github.com>
2026-06-24 21:49:58 -1000

    Added configuration and created initial development folder so that build always works. This is only for initial development.

---
+++ paskalON.OperatingModes/paskalON.OperatingModes.Domain/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using paskalON.Domains;
+
+namespace paskalON.OperatingModes.Domain
+{
+    /// <summary>
+    /// General configuration class for the microservice
+    /// </summary>
+    /// <remarks>
+    /// Each microservice might need some service configurations. E.g. archive URL of aggregated warranty data, audit table retention span, etc.
+    /// To achieve this and still have separation a configuration table must be created for each microservice with a key/value.
+    /// </remarks>
+    public class Configuration : ConfigurationBase
+    {
+        // This design is more a reminder that a configuration option exists.
+    }
+}
