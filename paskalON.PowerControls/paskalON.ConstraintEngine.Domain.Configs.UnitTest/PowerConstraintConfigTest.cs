c018ec318d97285dcceaa8b771972c772efbbb55
PKaelin <42666478+PKaelin@users.noreply.github.com>
2026-08-14 14:49:22 -1000

    Added ConstraintEngine.Domain.Configs.UnitTest project and tests.

---
+++ paskalON.PowerControls/paskalON.ConstraintEngine.Domain.Configs.UnitTest/PowerConstraintConfigTest.cs
@@ -0,0 +1,100 @@
+﻿// Copyright 2026 Pascal Kaelin (Operating as paskalON)
+// SPDX-License-Identifier: Apache-2.0
+//----------------------------------------‐------------------------------------
+namespace paskalON.ConstraintEngine.Domain.Configs.UnitTest
+{
+    [TestClass]
+    public sealed class PowerConstraintConfigTest
+    {
+        [TestMethod]
+        public void PowerConstraintMaxSmallerThanMinActiveTest()
+        {
+            PowerConstraintConfig config = new PowerConstraintConfig
+            {
+                ChangedBy = "Test",
+                Name = "PowerConstraintConfig",
+                MinimumActivePowerKiloWatt = 0
+            };
+
+            Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => config.MaximumActivePowerKiloWatt = -1);
+        }
+
+
+        [TestMethod]
+        public void PowerConstraintMinBiggerThanMaxActiveTest()
+        {
+            PowerConstraintConfig config = new PowerConstraintConfig
+            {
+                ChangedBy = "Test",
+                Name = "PowerConstraintConfig",
+                MaximumActivePowerKiloWatt = 0
+            };
+
+            Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => config.MinimumActivePowerKiloWatt = 1);
+        }
+
+
+        [TestMethod]
+        public void PowerConstraintMaxActiveTest()
+        {
+            PowerConstraintConfig config = new PowerConstraintConfig
+            {
+                ChangedBy = "Test",
+                Name = "PowerConstraintConfig",
+                MinimumActivePowerKiloWatt = -10,
+                MaximumActivePowerKiloWatt = 10
+            };
+
+            Assert.AreEqual(10, config.MaximumActivePowerKiloWatt);
+            Assert.AreEqual(-10, config.MinimumActivePowerKiloWatt);
+            Assert.IsNull(config.MaximumReactivePowerKiloVars);
+            Assert.IsNull(config.MinimumReactivePowerKiloVars);
+        }
+
+
+        [TestMethod]
+        public void PowerConstraintMaxSmallerThanMinReactiveTest()
+        {
+            PowerConstraintConfig config = new PowerConstraintConfig
+            {
+                ChangedBy = "Test",
+                Name = "PowerConstraintConfig",
+                MinimumReactivePowerKiloVars = 0
+            };
+
+            Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => config.MaximumReactivePowerKiloVars = -1);
+        }
+
+
+        [TestMethod]
+        public void PowerConstraintMinBiggerThanMaxReactiveTest()
+        {
+            PowerConstraintConfig config = new PowerConstraintConfig
+            {
+                ChangedBy = "Test",
+                Name = "PowerConstraintConfig",
+                MaximumReactivePowerKiloVars = 0
+            };
+
+            Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => config.MinimumReactivePowerKiloVars = 1);
+        }
+
+
+        [TestMethod]
+        public void PowerConstraintMaxReactiveTest()
+        {
+            PowerConstraintConfig config = new PowerConstraintConfig
+            {
+                ChangedBy = "Test",
+                Name = "PowerConstraintConfig",
+                MinimumReactivePowerKiloVars = -10,
+                MaximumReactivePowerKiloVars = 10
+            };
+
+            Assert.AreEqual(10, config.MaximumReactivePowerKiloVars);
+            Assert.AreEqual(-10, config.MinimumReactivePowerKiloVars);
+            Assert.IsNull(config.MaximumActivePowerKiloWatt);
+            Assert.IsNull(config.MinimumActivePowerKiloWatt);
+        }
+    }
+}
---
+++ paskalON.PowerControls/paskalON.ConstraintEngine.Domain.Configs.UnitTest/PowerRampConstraintConfigTest.cs
@@ -0,0 +1,54 @@
+﻿// Copyright 2026 Pascal Kaelin (Operating as paskalON)
+// SPDX-License-Identifier: Apache-2.0
+//----------------------------------------‐------------------------------------
+namespace paskalON.ConstraintEngine.Domain.Configs.UnitTest
+{
+    [TestClass]
+    public class PowerRampConstraintConfigTest
+    {
+        [TestMethod]
+        public void PowerRampConstraintMaxNegativeActiveTest()
+        {
+            Assert.ThrowsExactly<ArgumentOutOfRangeException>(() =>
+            {
+                new PowerRampConstraintConfig
+                {
+                    ChangedBy = "Test",
+                    Name = "PowerRampConstraintConfig",
+                    MaximumActivePowerKiloWattRampRatePerSecond = -1
+                };
+            });
+        }
+
+
+        [TestMethod]
+        public void PowerRampConstraintMaxNegativeReactiveTest()
+        {
+            Assert.ThrowsExactly<ArgumentOutOfRangeException>(() =>
+            {
+                new PowerRampConstraintConfig
+                {
+                    ChangedBy = "Test",
+                    Name = "PowerRampConstraintConfig",
+                    MaximumReactivePowerKiloVarsRampRatePerSecond = -1
+                };
+            });
+        }
+
+
+        [TestMethod]
+        public void PowerRampConstraintTest()
+        {
+            PowerRampConstraintConfig config = new PowerRampConstraintConfig
+            {
+                ChangedBy = "Test",
+                Name = "PowerRampConstraintConfig",
+                MaximumActivePowerKiloWattRampRatePerSecond = 1,
+                MaximumReactivePowerKiloVarsRampRatePerSecond = 2
+            };
+
+            Assert.AreEqual(1, config.MaximumActivePowerKiloWattRampRatePerSecond);
+            Assert.AreEqual(2, config.MaximumReactivePowerKiloVarsRampRatePerSecond);
+        }
+    }
+}
