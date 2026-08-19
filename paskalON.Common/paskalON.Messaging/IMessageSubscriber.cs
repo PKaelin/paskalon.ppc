7bbdc5fcc01e724cb07619e0315c1c823a3e9f30
PKaelin <42666478+PKaelin@users.noreply.github.com>
2026-08-18 14:53:24 -1000

    Added paskalON.Messaging project for the IMessageSuscbriber interface.

---
+++ paskalON.Common/paskalON.Messaging/IMessageSubscriber.cs
@@ -0,0 +1,15 @@
+﻿namespace paskalON.Messaging
+{
+    /// <summary>
+    /// Defines an interface for subscribing to message brokers.
+    /// </summary>
+    public interface IMessageSubscriber : IDisposable
+    {
+        /// <summary>
+        /// Subscribes a callback action to a specific message topic channel.
+        /// </summary>
+        /// <param name="topic">The name of the channel or topic to listen to.</param>
+        /// <param name="callback">The action executed when a message is received.</param>
+        void Subscribe(string topic, Action<string> callback);
+    }
+}
