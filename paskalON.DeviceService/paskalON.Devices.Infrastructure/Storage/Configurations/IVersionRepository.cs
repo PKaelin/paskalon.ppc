b6bcb63346feed1e1795ebbee2ec20aa83f1b5ea
PKaelin <42666478+PKaelin@users.noreply.github.com>
2026-08-24 11:17:25 -1000

    Added version repository.

---
+++ paskalON.DeviceService/paskalON.Devices.Infrastructure/Storage/Configurations/IVersionRepository.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2026 Pascal Kaelin (Operating as paskalON)
+// SPDX-License-Identifier: Apache-2.0
+//----------------------------------------‐------------------------------------
+namespace paskalON.Devices.Infrastructure.Storage.Configurations
+{
+    /// <summary>
+    /// Version repository interface definition.
+    /// </summary>
+    public interface IVersionRepository
+    {
+        /// <summary>
+        /// Gets the latest database version.
+        /// </summary>
+        /// <returns>The latest database version.</returns>
+        Task<string> GetDatabaseVersionAsync();
+    }
+}
---
+++ paskalON.DeviceService/paskalON.Devices.Infrastructure/Storage/VersionRepository.cs
@@ -0,0 +1,46 @@
+﻿// Copyright 2026 Pascal Kaelin (Operating as paskalON)
+// SPDX-License-Identifier: Apache-2.0
+//----------------------------------------‐------------------------------------
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using paskalON.Devices.Infrastructure.Storage.Repositories;
+using paskalON.Domains;
+
+namespace paskalON.Devices.Infrastructure.Storage
+{
+    /// <summary>
+    /// Version repository for getting version information.
+    /// </summary>
+    public class VersionRepository : RepositoryBase<DeviceServiceContext>
+    {
+        /// <summary>
+        /// Constructor of <see cref="VersionRepository"/>.
+        /// </summary>
+        /// <param name="logger">The logger interface for application logging and diagnostics.</param>
+        /// <param name="context">The device service database context.</param>
+        public VersionRepository(ILogger logger, DeviceServiceContext context)
+            : base(logger, context)
+        {
+        }
+
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public async Task<string> GetDatabaseVersionAsync()
+        {
+            History? history = null;
+
+            try
+            {
+                history = await Context.Histories.OrderBy(o => o.MigrationId).LastOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Error getting database version. Most likely there hasn't been a migration executed yet. {ex.Message}");
+            }
+
+            return history?.MigrationId ?? "No version history found";
+        }
+    }
+}
