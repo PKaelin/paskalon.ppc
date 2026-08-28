dc968397d98df053846e3f8288cf6f7f1b93fd56
PKaelin <42666478+PKaelin@users.noreply.github.com>
2026-08-28 11:41:54 -1000

    Added C37 and Modbus client structure.

---
+++ paskalON.Communication/paskalON.Protocols.C37118/C37Client.cs
@@ -0,0 +1,65 @@
+﻿// Copyright 2026 Pascal Kaelin (Operating as paskalON)
+// SPDX-License-Identifier: Apache-2.0
+//----------------------------------------‐------------------------------------
+using Microsoft.Extensions.Logging;
+using paskalON.Dataface.C37s;
+using paskalON.Protocols.C37118.Frames;
+
+namespace paskalON.Protocols.C37118
+{
+    public class C37Client : IC37Client
+    {
+        /// <summary>
+        /// Logger for application logging and diagnostics.
+        /// </summary>
+        private readonly ILogger<C37Client> _logger;
+
+        public C37ClientState State { get; set; } = C37ClientState.Disconnected;
+
+        public string ServerAddress { get; set; }
+
+        public int ServerPort { get; set; }
+
+        public event EventHandler<EventArgs>? OnCommunicationError;
+        public event EventHandler<C37DataFrameEventArgs>? DataFrameReceived;
+        public event EventHandler<C37ConfigFrameEventArgs>? ConfigFrameReceived;
+        public event EventHandler<Exception>? ConnectionError;
+
+
+        public C37Client(ILogger<C37Client> logger, string serverAddress, int serverPort)
+        {
+            ArgumentNullException.ThrowIfNull(logger);
+            ArgumentNullException.ThrowIfNull(serverAddress);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(serverPort);
+
+            _logger = logger;
+            ServerAddress = serverAddress;
+            ServerPort = serverPort;
+        }
+
+        public Task RequestConfigurationAsync()
+        {
+            OnCommunicationError?.Invoke(this, new EventArgs());
+            DataFrameReceived?.Invoke(this, new C37DataFrameEventArgs(null!));
+            ConfigFrameReceived?.Invoke(this, new C37ConfigFrameEventArgs(null!));
+            ConnectionError?.Invoke(this, null!);
+
+            return Task.CompletedTask;
+        }
+
+        public Task SendCommandAsync(C37CommandType command)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StartStreamingAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopStreamingAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ paskalON.Communication/paskalON.Protocols.Modbus/ModbusClient.cs
@@ -0,0 +1,106 @@
+﻿// Copyright 2026 Pascal Kaelin (Operating as paskalON)
+// SPDX-License-Identifier: Apache-2.0
+//----------------------------------------‐------------------------------------
+using Microsoft.Extensions.Logging;
+using paskalON.Dataface.Modbus;
+
+namespace paskalON.Protocols.Modbus
+{
+    public class ModbusClient : IModbusClient
+    {
+        /// <summary>
+        /// Logger for application logging and diagnostics.
+        /// </summary>
+        private readonly ILogger<ModbusClient> _logger;
+
+        public ModbusClientState State { get; set; } = ModbusClientState.Disconnected;
+
+        public string ServerAddress { get; init; }
+
+        public int ServerPort { get; init; }
+
+
+        public event EventHandler<EventArgs>? OnCommunicationError;
+
+
+        public ModbusClient(ILogger<ModbusClient> logger, string serverAddress, int serverPort)
+        {
+            ArgumentNullException.ThrowIfNull(logger);
+            ArgumentNullException.ThrowIfNull(serverAddress);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(serverPort);
+
+            _logger = logger;
+            ServerAddress = serverAddress;
+            ServerPort = serverPort;
+        }
+
+        public Task ConnectAsync(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                OnCommunicationError?.Invoke(this, new EventArgs());
+                _logger.LogError("Connect error occurred. Device: {ServerAddress}:{ServerPort}. {Error}:", ServerAddress, ServerPort, ex.Message);
+            }
+            return Task.CompletedTask;
+        }
+
+        public bool ConvertRawData(bool[] rawData, IModbusRegisterEntry register, ushort startAddress)
+        {
+            return true;
+        }
+
+        public object? ConvertRawData(ushort[] rawData, IModbusRegisterEntry register, ushort startAddress)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DisconnectAsync(CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<bool[]> ReadCoilsAsync(ushort startAddress, ushort endAddress, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(Array.Empty<bool>());
+        }
+
+        public Task<bool[]> ReadDiscreteInputsAsync(ushort startAddress, ushort endAddress, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(Array.Empty<bool>());
+        }
+
+        public Task<ushort[]> ReadHoldingRegistersAsync(ushort startAddress, ushort endAddress, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(Array.Empty<ushort>());
+        }
+
+        public Task<ushort[]> ReadInputRegistersAsync(ushort startAddress, ushort endAddress, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(Array.Empty<ushort>());
+        }
+
+        public Task WriteMultipleRegistersAsync(ushort address, ushort[] values, ModbusDataType type, short priority = 3, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task WriteSingleRegisterAsync(ushort address, double value, ModbusDataType type, short priority = 3, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task WriteSingleRegisterAsync(ushort address, ushort value, ModbusDataType type, short priority = 3, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task WriteSingleRegisterAsync(ushort address, bool value, ModbusDataType type, short priority = 3, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
