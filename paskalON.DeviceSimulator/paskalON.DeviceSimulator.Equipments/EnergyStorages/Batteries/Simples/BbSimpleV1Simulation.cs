da7d0a2a66502203b1d60f85d17c4d07ad4b97a0
PKaelin <42666478+PKaelin@users.noreply.github.com>
2026-09-16 14:53:48 -1000

    Added random walk endpoints for voltage and current for PCS and BB

--- paskalON.DeviceSimulator/paskalON.DeviceSimulator.Equipments/EnergyStorages/Batteries/Simples/BbSimpleV1Simulation.cs
+++ paskalON.DeviceSimulator/paskalON.DeviceSimulator.Equipments/EnergyStorages/Batteries/Simples/BbSimpleV1Simulation.cs
@@ -5,6 +5,7 @@
 using paskalON.Dataface.Modbus;
 using paskalON.Devices.Equipments.EnergyStorages.Batteries.Simples;
 using paskalON.DeviceSimulator.Equipments.Simulations;
+using paskalON.Maths.Randoms;
 using paskalON.Protocols.Modbus.Stores;
 using System.Diagnostics;
 
@@ -15,6 +16,18 @@ namespace paskalON.DeviceSimulator.Equipments.EnergyStorages.Batteries.Simples
     /// </summary>
     public sealed class BbSimpleV1Simulation : ISimulatedDevice
     {
+        /// <summary>
+        /// Random walker for frequency.
+        /// </summary>
+        private readonly RandomWalker<float> _sohWalker;
+
+
+        /// <summary>
+        /// Random walker for Voltage and Current.
+        /// </summary>
+        private readonly RandomWalker<float> _electricalWalker;
+
+
         /// <summary>
         /// Modbus data store for this device simulation.
         /// </summary>
@@ -55,6 +68,8 @@ namespace paskalON.DeviceSimulator.Equipments.EnergyStorages.Batteries.Simples
 
             _store = store;
             _device = device;
+            _sohWalker = new RandomWalker<float>(80, 0.1f, 60, 90, 5);
+            _electricalWalker = new RandomWalker<float>(500, 1, 100, 900, 50);
             // Start of with 100% preferred maximum
             double preferredStateOfCharge = 100;
             double absoluteStateOfCharge = device.PreferredMinimumStateOfCharge + (preferredStateOfCharge / device.AbsoluteMaximumStateOfCharge) *
@@ -113,6 +128,12 @@ namespace paskalON.DeviceSimulator.Equipments.EnergyStorages.Batteries.Simples
 
             RegisterSimulation.Write(_store, (int)BbSimpleV1Description.Register.TotalStateOfCharge, (ushort)UsableStateOfCharge,
                 ModbusDataType.MbUint16, ModbusScale.Upscale100);
+
+            //Write SOH
+            RegisterSimulation.Write(_store, (int)BbSimpleV1Description.Register.TotalStateOfHealth, _sohWalker.Next(), ModbusDataType.MbInt16, ModbusScale.Upscale100);
+            // Write electrical
+            RegisterSimulation.Write(_store, (int)BbSimpleV1Description.Register.TotalDCVoltage, _electricalWalker.Next(), ModbusDataType.MbInt16, ModbusScale.NoScale);
+            RegisterSimulation.Write(_store, (int)BbSimpleV1Description.Register.TotalDCCurrent, _electricalWalker.Next(), ModbusDataType.MbInt16, ModbusScale.NoScale);
 
             if (System.Diagnostics.Debugger.IsAttached == true)
             {
--- paskalON.DeviceSimulator/paskalON.DeviceSimulator.Equipments/PowerConversionSystems/Simples/PcsSimpleV1Simulation.cs
+++ paskalON.DeviceSimulator/paskalON.DeviceSimulator.Equipments/PowerConversionSystems/Simples/PcsSimpleV1Simulation.cs
@@ -6,6 +6,7 @@ using paskalON.Dataface.Modbus;
 using paskalON.Devices.Domain.Ders;
 using paskalON.Devices.Equipments.PowerConversionSystems.Simples;
 using paskalON.DeviceSimulator.Equipments.Simulations;
+using paskalON.Maths.Randoms;
 using paskalON.Protocols.Modbus.Stores;
 using System.Diagnostics;
 
@@ -16,6 +17,28 @@ namespace paskalON.DeviceSimulator.Equipments.PowerConversionSystems.Simples
     /// </summary>
     public sealed class PcsSimpleV1Simulation : ISimulatedDevice
     {
+        // TODO: read from configuration
+        private readonly float _referenceFrequency = 60;
+
+        /// <summary>
+        /// Random walker for frequency.
+        /// </summary>
+        private readonly RandomWalker<float> _frequencyWalker;
+
+
+        /// <summary>
+        /// Random walker for Voltage.
+        /// </summary>
+        private readonly RandomWalker<float> _voltageWalker;
+
+
+        /// <summary>
+        /// Random walker for Current.
+        /// </summary>
+        private readonly RandomWalker<float> _currentWalker;
+
+
+
         /// <summary>
         /// Modbus data store for this device simulation.
         /// </summary>
@@ -51,6 +74,9 @@ namespace paskalON.DeviceSimulator.Equipments.PowerConversionSystems.Simples
             _store = store;
             _device = device;
             _unit = (DerBatteryStorageUnit)device.DerUnit;
+            _frequencyWalker = new RandomWalker<float>(_referenceFrequency, 0.005f, _referenceFrequency - 0.03f, _referenceFrequency + 0.03f, 1);
+            _voltageWalker = new RandomWalker<float>(500, 1, 100, 900, 50);
+            _currentWalker = new RandomWalker<float>(100, 1, 10, 150, 50);
         }
 
 
@@ -82,8 +108,16 @@ namespace paskalON.DeviceSimulator.Equipments.PowerConversionSystems.Simples
             // For now we just set the nameplate
             RegisterSimulation.Write(_store, (int)PcsSimpleV1Description.Register.PAvailable, _device.NameplateMaximumActivePower.KiloWatts, ModbusDataType.MbInt16);
             RegisterSimulation.Write(_store, (int)PcsSimpleV1Description.Register.QAvailable, _device.NameplateMaximumReactivePower.KiloVoltAmperesReactive, ModbusDataType.MbInt16);
+            // Breakers and contactors are closed
             RegisterSimulation.Write(_store, (int)PcsSimpleV1Description.Register.ACBreaker, 1);
             RegisterSimulation.Write(_store, (int)PcsSimpleV1Description.Register.DcContactor, 1);
+            // Frequency
+            RegisterSimulation.Write(_store, (int)PcsSimpleV1Description.Register.Frequency, _frequencyWalker.Next(), ModbusDataType.MbInt16, ModbusScale.Upscale1000);
+            // Electrical
+            RegisterSimulation.Write(_store, (int)PcsSimpleV1Description.Register.DCCurrent, _currentWalker.Next(), ModbusDataType.MbInt16, ModbusScale.NoScale);
+            RegisterSimulation.Write(_store, (int)PcsSimpleV1Description.Register.DCVoltage, _voltageWalker.Next(), ModbusDataType.MbInt16, ModbusScale.NoScale);
+            RegisterSimulation.Write(_store, (int)PcsSimpleV1Description.Register.ACCurrent, _currentWalker.Next(), ModbusDataType.MbInt16, ModbusScale.NoScale);
+            RegisterSimulation.Write(_store, (int)PcsSimpleV1Description.Register.ACVoltage, _voltageWalker.Next(), ModbusDataType.MbInt16, ModbusScale.NoScale);
 
             if (System.Diagnostics.Debugger.IsAttached == true)
             {
