7f55f537fe9eb10d9372d325d691f1cf45315102
PKaelin <42666478+PKaelin@users.noreply.github.com>
2026-09-18 07:04:51 -1000

    Update the device simulator solution and add Application layer unit test structure

---
+++ paskalON.DeviceSimulator/paskalON.DeviceSimulator.Application.UnitTest/DeviceMapperSimulatorTest.cs
@@ -0,0 +1,11 @@
+﻿// Copyright 2026 Pascal Kaelin (Operating as paskalON)
+// Licensed under the paskalON Source-Available License (PSAL).
+// See LICENSE for the full license terms.
+//----------------------------------------‐------------------------------------
+namespace paskalON.DeviceSimulator.Application.UnitTest
+{
+    [TestClass]
+    public sealed class DeviceMapperSimulatorTest
+    {
+    }
+}
