f927d29193537d926664113182fcf8185d2a7cb1
PKaelin <42666478+PKaelin@users.noreply.github.com>
2026-09-20 09:03:23 -1000

    Added StateOfCharge calculations to physical units so as unit test for the StateOfCharge calculations.

---
+++ paskalON.Common/paskalON.PhysicalUnits.UnitTest/Percentages/StateOfChargeTest.cs
@@ -0,0 +1,69 @@
+﻿// Copyright 2026 Pascal Kaelin (Operating as paskalON)
+// Licensed under the paskalON Source-Available License (PSAL).
+// See LICENSE for the full license terms.
+//----------------------------------------‐------------------------------------
+using paskalON.PhysicalUnits.Percentages;
+
+namespace paskalON.PhysicalUnits.UnitTest.Percentages
+{
+    [TestClass]
+    public sealed class StateOfChargeTest
+    {
+        [TestMethod]
+        [DataRow(100, 20, 80, 100, 80)]
+        [DataRow(0, 20, 80, 100, 20)]
+        [DataRow(50, 20, 80, 100, 50)]
+        [DataRow(-16.67, 20, 80, 100, 10)]
+        [DataRow(116.67, 20, 80, 100, 90)]
+        [DataRow(-33.33, 20, 80, 100, 0)]
+        [DataRow(133.33, 20, 80, 100, 100)]
+        [DataRow(-50, 20, 80, 100, -10)]
+        [DataRow(150, 20, 80, 100, 110)]
+        public void GetAbsoluteStateOfChargeTest(double preferredSoc, double preferredMinimumSoc, double preferredMaximumSoc, double absoluteMaximumSoc, double expected)
+        {
+            double result = StateOfCharge.GetAbsoluteStateOfCharge(preferredSoc, preferredMinimumSoc, preferredMaximumSoc, absoluteMaximumSoc);
+
+            Assert.AreEqual(expected, Math.Round(result, 2));
+        }
+
+
+        [TestMethod]
+        [DataRow(0, 10, 90, 100, -12.5)]
+        [DataRow(10, 10, 90, 100, 0)]
+        [DataRow(20, 10, 90, 100, 12.5)]
+        [DataRow(30, 10, 90, 100, 25)]
+        [DataRow(40, 10, 90, 100, 37.5)]
+        [DataRow(50, 10, 90, 100, 50)]
+        [DataRow(60, 10, 90, 100, 62.5)]
+        [DataRow(70, 10, 90, 100, 75)]
+        [DataRow(80, 10, 90, 100, 87.5)]
+        [DataRow(90, 10, 90, 100, 100)]
+        [DataRow(100, 10, 90, 100, 112.5)]
+        public void GetUsableStateOfChargeTest(double absoluteSoc, double usableMinimumSoc, double usableMaximumSoc, double absoluteMaximumSoc, double expected)
+        {
+            double result = StateOfCharge.GetUsableStateOfCharge(absoluteSoc, usableMinimumSoc, usableMaximumSoc, absoluteMaximumSoc);
+
+            Assert.AreEqual(expected, Math.Round(result, 2));
+        }
+
+
+        [TestMethod]
+        [DataRow(5000000, 10, 90, 0, 100, 4000000)]
+        [DataRow(5000000, 20, 80, 0, 100, 3000000)]
+        [DataRow(5000000, 0, 100, 0, 100, 5000000)]
+        [DataRow(5000000, 10, 90, 10, 90, 5000000)]
+        [DataRow(1000000, 10, 90, 0, 100, 800000)]
+        [DataRow(10000000, 10, 90, 0, 100, 8000000)]
+        [DataRow(5000000, 25, 75, 0, 100, 2500000)]
+        [DataRow(5000000, 10, 80, 0, 100, 3500000)]
+        [DataRow(5000000, 20, 90, 0, 100, 3500000)]
+        public void GetUsableCapacityTest(double nameplateCapacity, double usableMinimumSoc,
+            double usableMaximumSoc, double absoluteMinimumSoc, double absoluteMaximumSoc, double expected)
+        {
+            double result = StateOfCharge.GetUsableCapacity(nameplateCapacity, usableMinimumSoc, usableMaximumSoc, absoluteMinimumSoc, absoluteMaximumSoc);
+
+            Assert.AreEqual(expected, Math.Round(result, 2));
+        }
+
+    }
+}
---
+++ paskalON.Common/paskalON.PhysicalUnits/Percentages/StateOfCharge.cs
@@ -0,0 +1,64 @@
+﻿// Copyright 2026 Pascal Kaelin (Operating as paskalON)
+// Licensed under the paskalON Source-Available License (PSAL).
+// See LICENSE for the full license terms.
+//----------------------------------------‐------------------------------------
+namespace paskalON.PhysicalUnits.Percentages
+{
+    public static class StateOfCharge
+    {
+        /// <summary>
+        /// Get the absolute State of Charge relative to the preferred State of Charge.
+        /// </summary>
+        /// <param name="preferredSoc">Preferred SOC.</param>
+        /// <param name="preferredMinimumStateOfCharge">Preferred Minimum SOC.</param>
+        /// <param name="preferredMaximumStateOfCharge">Preferred Maximum SOC.</param>
+        /// <param name="absoluteMaximumStateOfCharge">Absolute Maximum SOC./param>
+        /// <returns>Return the relative absolute State of Charge.</returns>
+        /// <remarks>
+        /// The preferred SOC is what the system/user sees so it should be always between 0% and 100%
+        /// The absolute is the physical boundary which should never be reached.
+        /// </remarks>
+        public static double GetAbsoluteStateOfCharge(double preferredSoc, double preferredMinimumStateOfCharge,
+            double preferredMaximumStateOfCharge, double absoluteMaximumStateOfCharge)
+        {
+            return preferredMinimumStateOfCharge + (preferredSoc / absoluteMaximumStateOfCharge) *
+                    (preferredMaximumStateOfCharge - preferredMinimumStateOfCharge);
+        }
+
+
+
+        /// <summary>
+        /// Get the usable State of Charge relative to the absolute State of Charge.
+        /// </summary>
+        /// <param name="absoluteSoc">Absolute SOC.</param>
+        /// <param name="usableMinimumStateOfCharge">Usable minimum SOC.</param>
+        /// <param name="usableMaximumStateOfCharge">Usable maximum SOC.</param>
+        /// <param name="absoluteMaximumStateOfCharge">Absolute maximum SOC.</param>
+        /// <returns>Returns the usable State of Charge.</returns>
+        /// The usable SOC is either the same as preferred SOC or outside the preferred SOC but within the absolute SOC.
+        /// The absolute is the physical boundary which should never be reached. 
+        public static double GetUsableStateOfCharge(double absoluteSoc, double usableMinimumStateOfCharge,
+            double usableMaximumStateOfCharge, double absoluteMaximumStateOfCharge)
+        {
+            return (absoluteSoc - usableMinimumStateOfCharge) /
+                (usableMaximumStateOfCharge - usableMinimumStateOfCharge) * absoluteMaximumStateOfCharge;
+        }
+
+
+        /// <summary>
+        /// Gets the usable capacity from the nameplate capacity.
+        /// </summary>
+        /// <param name="nameplateCapacity">The nameplate capacity.</param>
+        /// <param name="usableMinimumStateOfCharge">Usable minimum SOC.</param>
+        /// <param name="usableMaximumStateOfCharge">Usable maximum SOC.</param>
+        /// <param name="absoluteMinimumStateOfCharge">Absolute minimum SOC.</param>
+        /// <param name="absoluteMaximumStateOfCharge">Absolute maximum SOC.</param>
+        /// <returns>Returns the usable capacity.</returns>
+        public static double GetUsableCapacity(double nameplateCapacity, double usableMinimumStateOfCharge,
+            double usableMaximumStateOfCharge, double absoluteMinimumStateOfCharge, double absoluteMaximumStateOfCharge)
+        {
+            return nameplateCapacity * (usableMaximumStateOfCharge - usableMinimumStateOfCharge) /
+                (absoluteMaximumStateOfCharge - absoluteMinimumStateOfCharge);
+        }
+    }
+}
